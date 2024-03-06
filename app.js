ccf3abce4ae2befc9093f0c881c4d649dc36a602
Shivamshivpriye <shivamshivpriye@gmail.com>
2024-03-06 14:36:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+require("./db/conn");
+const cors = require("cors");
+const router = require("./Routes/router");
+const PORT = process.env.PORT || 6010
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads",express.static("./uploads"));
+app.use("/files",express.static("./public/files"));
+
+app.use(router);
+
+app.listen(PORT,()=>{
+    console.log(`Server start at port no ${PORT}`)
+})
